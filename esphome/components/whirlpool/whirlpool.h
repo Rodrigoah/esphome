087f201c72193e91e6ef971a9ac41fa291399115
Rodrigoah <Rodrigoah@users.noreply.github.com>
2024-07-29 18:38:11 +0200

    Update whirlpool.h

--- esphome/components/whirlpool/whirlpool.h
+++ esphome/components/whirlpool/whirlpool.h
@@ -7,19 +7,16 @@ namespace whirlpool {
 
 /// Simple enum to represent models.
 enum Model {
-  MODEL_DG11J1_3A = 0,  /// Temperature range is from 18 to 32
   MODEL_DG11J1_91 = 1,  /// Temperature range is from 16 to 30
 };
 
 // Temperature
-const float WHIRLPOOL_DG11J1_3A_TEMP_MAX = 32.0;
-const float WHIRLPOOL_DG11J1_3A_TEMP_MIN = 18.0;
-const float WHIRLPOOL_DG11J1_91_TEMP_MAX = 30.0;
-const float WHIRLPOOL_DG11J1_91_TEMP_MIN = 16.0;
+const float whirlpool_DG11J1_91_TEMP_MAX = 30.0;
+const float whirlpool_DG11J1_91_TEMP_MIN = 16.0;
 
-class WhirlpoolClimate : public climate_ir::ClimateIR {
+class whirlpoolClimate : public climate_ir::ClimateIR {
  public:
-  WhirlpoolClimate()
+  whirlpoolClimate()
       : climate_ir::ClimateIR(temperature_min_(), temperature_max_(), 1.0f, true, true,
                               {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM,
                                climate::CLIMATE_FAN_HIGH},
@@ -54,10 +51,10 @@ class WhirlpoolClimate : public climate_ir::ClimateIR {
   Model model_;
 
   float temperature_min_() {
-    return (model_ == MODEL_DG11J1_3A) ? WHIRLPOOL_DG11J1_3A_TEMP_MIN : WHIRLPOOL_DG11J1_91_TEMP_MIN;
+    return (model_ == MODEL_DG11J1_3A) ? whirlpool_DG11J1_3A_TEMP_MIN : whirlpool_DG11J1_91_TEMP_MIN;
   }
   float temperature_max_() {
-    return (model_ == MODEL_DG11J1_3A) ? WHIRLPOOL_DG11J1_3A_TEMP_MAX : WHIRLPOOL_DG11J1_91_TEMP_MAX;
+    return (model_ == MODEL_DG11J1_3A) ? whirlpool_DG11J1_3A_TEMP_MAX : whirlpool_DG11J1_91_TEMP_MAX;
   }
 };
 
