d1d222e8a74f8dda471261657875c9dfcd2c4425
Rodrigoah <Rodrigoah@users.noreply.github.com>
2024-07-30 17:32:01 +0200

    Update whirlpool.cpp

--- esphome/components/whirlpool/whirlpool.cpp
+++ esphome/components/whirlpool/whirlpool.cpp
@@ -44,8 +44,8 @@ void WhirlpoolClimate::transmit_state() {
   auto powered_on = this->mode != climate::CLIMATE_MODE_OFF;
   if (powered_on != this->powered_on_assumed) {
     // Set power toggle command
-    remote_state[2] = 4;
-    remote_state[15] = 1;
+    //remote_state[2] = 4;
+   // remote_state[15] = 1;
     this->powered_on_assumed = powered_on;
   }
   switch (this->mode) {
