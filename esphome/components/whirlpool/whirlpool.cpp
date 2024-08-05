70326018184e6dc61a1a1d570aa5c002b1dfa740
Rodrigoah <Rodrigoah@users.noreply.github.com>
2024-08-05 18:39:45 +0200

    Update whirlpool.cpp

--- esphome/components/whirlpool/whirlpool.cpp
+++ esphome/components/whirlpool/whirlpool.cpp
@@ -84,8 +84,8 @@ void WhirlpoolClimate::transmit_state() {
   }
 
   // Temperature
-  auto temp = (uint8_t) roundf(clamp(this->target_temperature, this->temperature_min_(), this->temperature_max_()));
-  remote_state[3] |= (uint8_t) (temp - this->temperature_min_()) << 4;
+  auto temp = (uint8_t) roundf(clamp(this->target_temperature, this->MIN_TEMP , this->MAX_TEMP ));
+  remote_state[3] |= (uint8_t) (temp - this->MIN_TEMP) << 4;
 
   // Fan speed
   switch (this->fan_mode.value()) {
